b42a2dda22d2b21fbb02d4be75ab84d087304f25
TommyVisch <58881184+TommyVisch@users.noreply.github.com>
2023-12-03 16:43:13 +0100

    personal changes

--- parameters.py
+++ parameters.py
@@ -4,8 +4,8 @@ from datetime import datetime
 # News Scraping
 WEBSITES = ["cnbc.com", "reuters.com"]
 QUERIES = ["AAPL", "MSFT", "AMZN", "NVDA", "TSLA"]
-START_DATE = datetime(2023,12,31) # Inclusive
-END_DATE = datetime(2023,12,31) # Exclusive
+START_DATE = datetime(2023,12,1) # Inclusive
+END_DATE = datetime(2023,12,3) # Exclusive
 DAYS_PER_QUERY = 14
-MIN_DELAY = 1
-MAX_DELAY = 2
+MIN_DELAY = 5
+MAX_DELAY = 10
